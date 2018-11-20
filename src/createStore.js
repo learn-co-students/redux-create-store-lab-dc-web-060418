690a33fbd80bcb468da719dcd9308c6372cb80c6
j-shilling <shilling.jake@gmail.com>
2018-11-20 13:10:27 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,15 @@
 export default function createStore(reducer) {
   // add your code here
+    let state = reducer(undefined, {type: "@@INIT"});
+    function getState() {
+	return state;
+    }
+    function dispatch(action) {
+	state = reducer(state, action);
+	render();
+    }
+
+    return { getState, dispatch };
 }
 
 function render() {
